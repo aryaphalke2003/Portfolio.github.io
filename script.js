5c919841b3062058112b436f9d580aa889e276c4
aryaphalke2003 <aryaphalke2003@gmail.com>
2023-09-24 04:02:00 +0530

    projects backgnd

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let theme = localStorage.getItem('theme')
 
 if (theme == null)
 {
-    setTheme('light')   
+    setTheme('blue')   
 }
 else {
     setTheme(theme)
@@ -18,10 +18,10 @@ for (var i = 0; themeDots.length > i; i++){
     })
 }
 function setTheme(mode) {
-    if (mode == 'light')
-    {
-        document.getElementById('theme-style').href='default.css'
-    }
+    // if (mode == 'light')
+    // {
+    //     document.getElementById('theme-style').href='default.css'
+    // }
     if (mode == 'blue') {
         document.getElementById('theme-style').href = 'blue.css'
     }
